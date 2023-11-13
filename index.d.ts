e79dfe1f68e3651824165a86ed7b900560348f0d
Ali Camarata <alisalaah@gmail.com>
2023-11-13 09:16:11 +0700

    updated types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare module 'praycalc' {
     export function getMoon(date: Date): MoonReturnType;
-    export function getTimes(date: Date, lat: number, lng: number, elevation?: number, temperature?: number, pressure?: number): TimesReturnType;
-    export function calcTimes(date: Date, lat: number, lng: number, elevation?: number, temperature?: number, pressure?: number): CalcTimesReturnType;
-    export function getTimesAll(date: Date, lat: number, lng: number, elevation?: number, temperature?: number, pressure?: number): TimesAllReturnType;
-    export function calcTimesAll(date: Date, lat: number, lng: number, elevation?: number, temperature?: number, pressure?: number): CalcTimesAllReturnType;
+    export function getTimes(date: Date, lat: number, lng: number, tz: number, elevation?: number, temperature?: number, pressure?: number): TimesReturnType;
+    export function calcTimes(date: Date, lat: number, lng: number, tz: number, elevation?: number, temperature?: number, pressure?: number): CalcTimesReturnType;
+    export function getTimesAll(date: Date, lat: number, lng: number, tz: number, elevation?: number, temperature?: number, pressure?: number): TimesAllReturnType;
+    export function calcTimesAll(date: Date, lat: number, lng: number, tz: number, elevation?: number, temperature?: number, pressure?: number): CalcTimesAllReturnType;
   
     interface MoonReturnType {
         fraction: number;
