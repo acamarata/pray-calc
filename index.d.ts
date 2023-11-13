d7c449ece7b889b79dff3c7eae660cbc6b443eb0
Ali Camarata <alisalaah@gmail.com>
2023-11-13 22:39:50 +0700

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,31 @@
+// index.d.ts
 declare module 'praycalc' {
-    export function getMoon(date: Date): MoonReturnType;
+    export function getMoon(date: Date, latitude?: number, longitude?: number, elevation?: number, temp?: number, pressure?: number, humidity?: number, clouds?: number): MoonDetails;  
     export function getTimes(date: Date, lat: number, lng: number, tz: number, elevation?: number, temperature?: number, pressure?: number): TimesReturnType;
     export function calcTimes(date: Date, lat: number, lng: number, tz: number, elevation?: number, temperature?: number, pressure?: number): CalcTimesReturnType;
     export function getTimesAll(date: Date, lat: number, lng: number, tz: number, elevation?: number, temperature?: number, pressure?: number): TimesAllReturnType;
     export function calcTimesAll(date: Date, lat: number, lng: number, tz: number, elevation?: number, temperature?: number, pressure?: number): CalcTimesAllReturnType;
-  
-    interface MoonReturnType {
-        fraction: number;
-        phase: number;
-        angle: number;
+
+    interface MoonPosition {
+      azimuth: number;
+      altitude: number;
+      distance: number;
+      parallacticAngle: number;
+    }
+
+    interface MoonIllumination {
+      fraction: number;
+      phase: number;
+      angle: number;
+    }
+
+    interface MoonDetails {
+      phase: number;
+      phaseName: string;
+      phaseSymbol: string;
+      position: MoonPosition;
+      illumination: MoonIllumination;
+      visibility: number;
     }
 
     interface TimesReturnType {
